23be54d0975d19d2e8649203dce6fb9040f4cd5f
henry <henriquy1224@gmail.com>
2024-09-15 14:29:24 -0300

    refactor: :art: limpeza de código

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String input = sc.nextLine();
+        // 2, Beltrano, dos Santos, beltrano@hotmail.com, Rua Tal, 456, Centro, Ibirá, SP, Brasil, Brasileiro
 
         String inputSeparado[] = input.split(", ");
 
-        // 2, Beltrano, dos Santos, beltrano@hotmail.com, Rua Tal, 456, Centro, Ibirá, SP, Brasil, Brasileiro
         String idPessoa = inputSeparado[0];
         String nome = inputSeparado[1];
-        String sobrenome = inputSeparado[2];;
+        String sobrenome = inputSeparado[2];
         String email = inputSeparado[3];
-        String logradouro = inputSeparado[4];;
+        String logradouro = inputSeparado[4];
         String numero = inputSeparado[5];
         String bairro = inputSeparado[6];
         String cidade = inputSeparado[7];
         String estado = inputSeparado[8];
         String pais = inputSeparado[9];
-        String nacionalidade = inputSeparado[10];;
+        String nacionalidade = inputSeparado[10];
         Pessoa pessoa = new Pessoa(Integer.parseInt(idPessoa), nome, sobrenome, email, logradouro, numero, bairro, cidade, estado, pais, nacionalidade);
         sc.close();
 
